97737d28eab2c70fed96d48203ab6409e5583ccb
M SAQIB ALI <MSaqibALIISF@gmail.com>
2025-05-19 14:49:32 +0500

    feat: add Reverb configuration constants and broadcasting route to constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,21 @@
 export const USER_COOKIE_KEY = "gshield_user";
 
+export const REVERB_APP_ID = +process.env.REVERB_APP_ID!;
+export const REVERB_APP_KEY = process.env.REVERB_APP_KEY!;
+export const REVERB_APP_SECRET = process.env.REVERB_APP_SECRET!;
+export const REVERB_HOST = process.env.REVERB_HOST!;
+// export const REVERB_HOST =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.REVERB_HOST!
+//     : "localhost";
+export const REVERB_PORT = +process.env.REVERB_PORT!;
+export const REVERB_SCHEME = process.env.REVERB_SCHEME!;
+// export const REVERB_SCHEME =
+//   process.env.NODE_ENV === "production" ? "https" : "http";
+
 export const STRIPE_PUBLISHABLE_KEY =
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
-export const STRIPE_SECRET_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!;
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY!;
 
 export const REST_API_BASE_URL = process.env.NEXT_PUBLIC_REST_API_BASE_URL!;
 
@@ -54,3 +67,6 @@ export const VIEW_SUPPORT_TICKET_ROUTE = (ticketId: number) =>
 
 export const MESSAGE_REPLY_SUPPORT_TICKET_ROUTE = (ticketId: number) =>
   `${REST_API_BASE_URL}/ticket/${ticketId}/reply`;
+
+export const MESSAGE_BROADCASTING_ROUTE =
+  REST_API_BASE_URL + "/broadcasting/auth";
